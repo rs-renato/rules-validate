7537af92d94498f520c29ef3a501e399a4249517
Renato Rodrigues <spamcares-github@yahoo.com>
2016-02-29 20:49:09 -0300

    API javadoc definition

--- src/test/java/br/com/usage/message/Messages.java
+++ src/test/java/br/com/usage/message/Messages.java
@@ -2,10 +2,6 @@ package br.com.usage.message;
 
 import br.com.enums.ValidateableMessages;
 
-/**
- * Defines all validation message to be showed in validate phase,
- * used in {@link br.com.rules.RuleValidation}
- */
 public enum Messages implements ValidateableMessages {
 
 	REJECT_590(590, "590 message description error"),
@@ -14,29 +10,16 @@ public enum Messages implements ValidateableMessages {
 	private Integer code;
 	private String message;
 
-    /**
-     * Validation message constructor
-     * @param code code of this validation message
-     * @param message message to this validation message
-     */
     Messages(Integer code, String message){
 		this.code = code;
 		this.message = message;
 	}
 
-    /**
-     * Retrieves the code of this {@link Messages}
-     * @return code
-     */
 	public Integer getCode(){
 		return this.code;
 	}
 
-    /**
-     * Retrieves the message of this {@link Messages}
-     * @return message
-     */
-	public String getMessage(){
+    public String getMessage(){
 		return this.message;
 	}
 }
